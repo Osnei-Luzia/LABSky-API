d4063ed326eebe893d204f5c1812c63021999a53
Osnei Luzia <osnei.luzia@gmail.com>
2023-05-30 11:21:04 -0300

    Setup Inicial

---
+++ projeto/src/main/java/com/backend/labskyapi/LabSkyApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.labskyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSkyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSkyApiApplication.class, args);
+    }
+
+}
---
+++ projeto/src/test/java/com/backend/labskyapi/LabSkyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.labskyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSkyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
