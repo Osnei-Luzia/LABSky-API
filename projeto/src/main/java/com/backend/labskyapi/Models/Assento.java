d3c7a3e79198a1d93f8b53ea174a2f0fc807e9e5
Osnei Luzia <osnei.luzia@gmail.com>
2023-05-30 12:24:50 -0300

    Carga inicial

--- projeto/src/main/java/com/backend/labskyapi/Models/Assento.java
+++ projeto/src/main/java/com/backend/labskyapi/Models/Assento.java
@@ -1,11 +1,16 @@
 package com.backend.labskyapi.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "assentos")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Assento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- projeto/src/main/java/com/backend/labskyapi/Models/Passageiro.java
+++ projeto/src/main/java/com/backend/labskyapi/Models/Passageiro.java
@@ -1,14 +1,19 @@
 package com.backend.labskyapi.Models;
 
 import com.backend.labskyapi.Models.Enums.Classificacao;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "passageiros")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Passageiro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
